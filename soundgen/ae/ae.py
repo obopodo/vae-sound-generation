50c3969fa3838881e733c1dc96cd555bced9f36a
Boris Podolny <bpodolnyi@gmail.com>
2025-08-20 21:46:50 +0300

    add decoder

--- soundgen/ae/ae.py
+++ soundgen/ae/ae.py
@@ -1,31 +1,51 @@
-from sympy import flatten
+import numpy as np
 from torch import nn
 from torchinfo import summary
 
+from soundgen.utils import calculate_conv2d_output_shape
+
 
 class Autoencoder(nn.Module):
+
     def __init__(
         self,
-        input_channels: int,
+        input_shape: int,
         conv_filters_number: list[int],
         conv_kernel_size: list[int],
         conv_strides: list[int],
         latent_space_dim: int,
+        padding: int = 1,
     ):
         super().__init__()
-        self.input_channels = input_channels  # num_channels
+        self.input_shape = input_shape  # [num_channels, height, width]
+        self.num_channels = input_shape[0]
         self.conv_filters_number = conv_filters_number  # number of filters in each conv layer, eg [2, 4, 8]
         self.conv_kernel_size = conv_kernel_size  # kernel size for each conv layer, eg [3, 5, 3]
         self.conv_strides = conv_strides  # stride for each conv layer, eg [1, 2, 2]
         self.latent_space_dim = latent_space_dim
+        self.padding = padding
 
         self.encoder = nn.Sequential()
         self.decoder = nn.Sequential()
-        # self.model = nn.Sequential(self.encoder, self.decoder)
-        self.model = nn.Sequential(self.encoder)
+        # self.model = nn.Sequential(self.encoder)
+        self.model = nn.Sequential(self.encoder, self.decoder)
 
         self._num_conv_layers = len(conv_filters_number)
+        self._shape_before_bottleneck = self._get_shape_before_bottleneck()
+        self._linear_layer_size = np.prod(
+            self._shape_before_bottleneck
+        )  # size of the flattened layer after convolutions
         self._build()
+
+    def _get_shape_before_bottleneck(self):
+        """Calculate the size of the flattened layer after convolutions.
+
+        The same will be used for the layer output size after the bottleneck.
+        """
+        size = self.input_shape[1:]  # the number of input channels doesn't matter
+        for kernel_size, stride in zip(self.conv_kernel_size, self.conv_strides):
+            size = calculate_conv2d_output_shape(size, kernel_size, padding=self.padding, stride=stride)
+        return self.conv_filters_number[-1], size[0], size[1]  # (last_conv_channels, height, width)
 
     def forward(self, x):
         x = self.model(x)
@@ -43,7 +63,7 @@ class Autoencoder(nn.Module):
     def _add_convolutions(self):
         for i in range(self._num_conv_layers):
             if i == 0:
-                in_channels = self.input_channels
+                in_channels = self.num_channels
             else:
                 in_channels = self.conv_filters_number[i - 1]
 
@@ -53,7 +73,7 @@ class Autoencoder(nn.Module):
                     out_channels=self.conv_filters_number[i],
                     kernel_size=self.conv_kernel_size[i],
                     stride=self.conv_strides[i],
-                    padding=1,
+                    padding=self.padding,
                 )
             )
             self.encoder.append(nn.ReLU())
@@ -61,10 +81,51 @@ class Autoencoder(nn.Module):
 
     def _add_bottleneck(self):
         self.encoder.append(nn.Flatten())
-        self.encoder.append(nn.LazyLinear(self.latent_space_dim))
+        # self.encoder.append(nn.LazyLinear(self.latent_space_dim))
+        self.encoder.append(nn.Linear(self._linear_layer_size, self.latent_space_dim))
 
     def _build_decoder(self):
-        pass
+        # self.decoder.append(nn.Linear(self.latent_space_dim, ))
+        self._add_dense_layer()
+        self._add_reshape_layer()
+        self._add_conv_transpose_layers()
+        self._add_decoder_output()
+
+    def _add_dense_layer(self):
+        self.decoder.append(nn.Linear(self.latent_space_dim, self._linear_layer_size))
+
+    def _add_reshape_layer(self):
+        self.decoder.append(nn.Unflatten(1, self._shape_before_bottleneck))
+
+    def _add_conv_transpose_layers(self):
+        """Loop through the all conv layers in reverse order except the first one."""
+        for i in reversed(range(1, self._num_conv_layers)):
+            self.decoder.append(
+                nn.ConvTranspose2d(
+                    in_channels=self.conv_filters_number[i],  # ?
+                    out_channels=self.conv_filters_number[i - 1],  # ?
+                    kernel_size=self.conv_kernel_size[i],
+                    stride=self.conv_strides[i],
+                    padding=self.padding,
+                    output_padding=(1 if self.conv_strides[i] > 1 else 0),
+                )
+                # output padding is used to ensure the output shape matches the input shape
+                # IDK how to calculate the exact value, 1 just worked for stride equal to 1 and 2
+            )
+            self.decoder.append(nn.ReLU())
+            self.decoder.append(nn.BatchNorm2d(self.conv_filters_number[i - 1]))
+
+    def _add_decoder_output(self):
+        self.decoder.append(
+            nn.ConvTranspose2d(
+                in_channels=self.conv_filters_number[0],
+                out_channels=self.num_channels,
+                kernel_size=self.conv_kernel_size[0],
+                stride=self.conv_strides[0],
+                padding=self.padding,
+            )
+        )
+        self.decoder.append(nn.Sigmoid())  # Use Sigmoid for output layer to normalize the output
 
     def _build_autoencoder(self):
         pass
@@ -72,7 +133,7 @@ class Autoencoder(nn.Module):
 
 if __name__ == "__main__":
     model = Autoencoder(
-        input_channels=1,
+        input_shape=[1, 28, 28],
         conv_filters_number=[32, 64, 64, 64],
         conv_kernel_size=[3, 3, 3, 3],
         conv_strides=[1, 2, 2, 1],
--- soundgen/utils.py
+++ soundgen/utils.py
@@ -10,11 +10,6 @@ def get_device() -> str:
         return "cpu"
 
 
-def calculate_conv1d_output_shape(input_length: int, kernel_size: int, *, padding: int = 0, stride: int = 1) -> int:
-    """Conv Output Size = (Input Size - Kernel Size + 2 * Padding) / Stride + 1"""
-    return (input_length - kernel_size + 2 * padding) // stride + 1
-
-
 def calculate_conv2d_output_shape(
     input_shape: tuple[int, int],
     kernel_size: int,
@@ -28,6 +23,7 @@ def calculate_conv2d_output_shape(
     Conv Output Size = (Input Size - Kernel Size + 2 * Padding) / Stride + 1
     Example: (64, 44) -> (16, 11)
     """
+    # TODO: consider adding pooling into this formula
     height, width = input_shape
     output_height = (height - kernel_size + 2 * padding) // stride + 1
     output_width = (width - kernel_size + 2 * padding) // stride + 1
@@ -40,4 +36,5 @@ if __name__ == "__main__":
     x = calculate_conv2d_output_shape(x, 3, padding=1, stride=1)
     x = calculate_conv2d_output_shape(x, 3, padding=1, stride=2)
     x = calculate_conv2d_output_shape(x, 3, padding=1, stride=2)
+    assert x == (16, 11), f"Expected (16, 11), got {x}"
     print(x)
