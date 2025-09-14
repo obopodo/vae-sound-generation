e0bb5351082f0e4029190f629769a6f6d8531a46
Boris Podolny <bpodolnyi@gmail.com>
2025-09-14 16:03:01 +0300

    add scripts for both models

---
+++ soundgen/scripts/test_script.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from matplotlib.axes import Axes
+from torchinfo import summary
+
+from soundgen.ae import Autoencoder
+from soundgen.ae.train_ae import load_mnist_data
+from soundgen.vae import VAE
+
+
+def tensor_to_image(image: torch.Tensor) -> np.ndarray:
+    """Convert a tensor to a numpy image supported by matplotlib.
+
+    squeeze(0) removes batch dimension
+    permute(1, 2, 0) makes channel dimension last
+    """
+    return image.squeeze(0).permute(1, 2, 0).detach().numpy()
+
+
+def plot_one_sample(real_image: torch.Tensor, reconstructed: torch.Tensor, label: int, ax: Axes):
+    real_image = tensor_to_image(real_image)
+    reconstructed = tensor_to_image(reconstructed)
+    ax[0].imshow(real_image, cmap="gray")
+    ax[0].set_title(f"{label}: Original Image")
+    ax[1].imshow(reconstructed, cmap="gray")
+    ax[1].set_title(f"Reconstructed Image")
+
+
+if __name__ == "__main__":
+    N_SAMPLES = 10
+    RANDOM_SEED = 73
+    MODEL_CLASS = VAE  # VAE or Autoencoder
+
+    top_folder = Path("/Users/borispodolnyi/Documents/coding_projects/vae_sound_generation/models")
+    weights_path = top_folder / "checkpoint_e17_20250914_155332.pth"
+    params_path = top_folder / "vae_mnist_20250914_154615.json"
+
+    model = MODEL_CLASS.load(weights_path, params_path)
+    # summary(model, input_size=[1] + list(model.input_shape))
+
+    _, test_dataset = load_mnist_data(root="./data", return_loaders=False)
+
+    np.random.seed(RANDOM_SEED)
+    _, ax = plt.subplots(2, N_SAMPLES, figsize=(6 * N_SAMPLES, 4))
+    for plot_index, sample_index in enumerate(np.random.randint(len(test_dataset), size=N_SAMPLES)):
+        image, label = test_dataset[sample_index]
+        image = image.unsqueeze(0)
+        reconstructed_image = model(image)
+        plot_one_sample(image, reconstructed_image, label, ax[:, plot_index])
+    plt.tight_layout()
+    plt.show()
---
+++ soundgen/scripts/train_script.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+
+from torch import nn
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+from soundgen.ae import Autoencoder
+from soundgen.scripts.test_script import MODEL_CLASS
+from soundgen.train import train
+from soundgen.utils import get_device
+from soundgen.vae import VAE
+
+
+def load_mnist_data(root="./data", batch_size: int = 4, return_loaders: bool = True) -> tuple[DataLoader, DataLoader]:
+    train = datasets.MNIST(root=root, train=True, download=True, transform=ToTensor())
+    test = datasets.MNIST(root=root, train=False, download=True, transform=ToTensor())
+    if not return_loaders:
+        return train, test
+
+    train = DataLoader(train, batch_size=batch_size, shuffle=True)
+    test = DataLoader(test, batch_size=batch_size, shuffle=False)
+    return train, test
+
+
+if __name__ == "__main__":
+    BATCH_SIZE = 32
+    LEARNING_RATE = 0.0005
+    EPOCHS = 20
+    MODEL_FILE_PREFIX = "vae_mnist"
+    MODEL_CLASS = VAE  # VAE or Autoencoder
+
+    train_data_loader, valid_data_loader = load_mnist_data(batch_size=BATCH_SIZE)
+
+    model = MODEL_CLASS(
+        input_shape=[1, 28, 28],
+        conv_filters_number=[32, 64, 64, 64],
+        conv_kernel_size=[3, 3, 3, 3],
+        conv_strides=[1, 2, 2, 1],
+        latent_space_dim=2,
+    )
+
+    device = get_device()
+    model = model.to(device)
+    print("Using device:", device)
+
+    loss = nn.MSELoss()
+    optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
+
+    model.save_parameters(f"./models/{MODEL_FILE_PREFIX}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json")
+    train(
+        model=model,
+        train_data_loader=train_data_loader,
+        valid_data_loader=valid_data_loader,
+        loss_fn=loss,
+        optimizer=optimizer,
+        device=device,
+        epochs=EPOCHS,
+        save_checkpoint=True,
+    )
